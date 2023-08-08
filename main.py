b01af78c45b5db7962de22fae1742a3acdaa25d7
EletrixTime <71174682+EletrixtimeYT@users.noreply.github.com>
2023-08-08 18:40:37 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,83 @@
+import os
+import yaml
+import config
+print("TimeWeb starting...")
+def is_file_allowed(filename):
+    exception_path = "config/exception.yaml"
+    if os.path.exists(exception_path):
+        with open(exception_path, "r") as exception_file:
+            exceptions = yaml.safe_load(exception_file)
+            if filename in exceptions:
+                return False
+    return True
+
+if not os.path.exists("dontremoveme.txt"):
+    print("===========")
+    print("Downloader")
+    print("===========")
+    print("1/5 Installing required packages")
+    os.system("pip install flask")
+    os.system("pip install git+https://github.com/eletrixtimeyt/flask-monitoringdashboard")
+    os.system("pip install requests")
+    os.system("pip install pyyaml")
+    with open("dontremoveme.txt", "w") as txt:
+        txt.write("Dont remove me.\n")
+        txt.write("You can remove me for an update!\n")
+    print("2/5 Creating templates folder")
+    if not os.path.exists("public"):
+        os.makedirs("public")
+    print("3/5 Creating config folder")
+    if not os.path.exists("config"):
+        os.makedirs("config")
+        # Créer le fichier exception.yaml s'il n'existe pas
+    exception_path = "config/exception.yaml"
+    if not os.path.exists(exception_path):
+        with open(exception_path, "w") as exception_file:
+            temp1 = """
+
+# List of files that are not allowed to be accessed\n
+
+#==========================================\n
+# #DONT REMOVE IS FOR SAFETY OF THE SERVER\n
+- flask_monitoringdashboard.db\n
+- main.py\n
+- config/exception.yaml\n
+
+#==========================================\n
+#Your can add your rules here !\n
+
+#- filename.txt\n
+"""
+            exception_file.write(temp1)
+    response = requests.get("https://raw.githubusercontent.com/EletrixTimeYT/TimeWeb/main/public/index.html")
+    response.raise_for_status()
+    with open("public/index.html", "w", encoding="utf-8") as f:
+        f.write(response.text)
+   
+else:
+    print("1/5 Required packages already installed")
+
+print("4/5 Finishing something...")
+
+print("5/5 Starting...")
+print("NOTE: Put HTML files in the templates folder!")
+
+from flask import Flask, send_file, abort 
+import flask_monitoringdashboard as dashboard
+
+app = Flask(__name__)
+dashboard.bind(app)
+
+@app.route('/<path:filename>')  # Use a dynamic path to capture the entire path after "/"
+def get_asset(filename):
+    if not is_file_allowed(filename):
+        return abort(401)  # Return a 401 error if the file is in the exceptions list
+    
+    public_filename = os.path.join("public", filename)
+    return send_file(public_filename)
+@app.route('/')
+def index():
+    return send_file("public/index.html")
+
+if __name__ == '__main__':
+    app.run(port=config.port)
---
+++ tools.py
@@ -0,0 +1,111 @@
+import os
+
+os.system("pip install pyyaml requests")
+import yaml
+import requests
+import shutil
+import argparse
+import subprocess
+
+def get_local_version():
+    version_path = "config/version.yaml"
+    
+    if os.path.exists(version_path):
+        with open(version_path, "r") as version_file:
+            current_version = yaml.safe_load(version_file).get("version", "")
+        return current_version
+    return ""
+
+def update_local_version(new_version):
+    version_path = "config/version.yaml"
+    with open(version_path, "w") as version_file:
+        yaml.dump({"version": new_version}, version_file)
+
+def execute_commands(commands):
+    for command in commands:
+        try:
+            subprocess.run(command, shell=True, check=True)
+        except subprocess.CalledProcessError:
+            print(f"Failed to execute command: {command}")
+
+def install_files():
+    download_files = ["main.py", "version.yaml", "config.py"]  # Ajoutez les fichiers à télécharger ici
+    
+    for file in download_files:
+        download_url = f"https://raw.githubusercontent.com/EletrixTimeYT/TimeWeb/main/{file}"
+        try:
+            response = requests.get(download_url, stream=True)
+            response.raise_for_status()
+            with open(file, "wb") as f:
+                response.raw.decode_content = True
+                shutil.copyfileobj(response.raw, f)
+        except (requests.exceptions.RequestException, IOError):
+            print(f"Unable to download {file} from GitHub.")
+
+def check_update(update_libs=False, install=False):
+    current_version = get_local_version()
+
+    github_url = "https://raw.githubusercontent.com/EletrixTimeYT/TimeWeb/main/version.yaml"  # Mettez à jour avec votre URL GitHub
+    try:
+        response = requests.get(github_url)
+        response.raise_for_status()
+        data = response.json()
+        github_version = data.get("version", "")
+        update_files = data.get("update_files", [])
+        update_libs_list = data.get("update_libs", [])
+        update_commands = data.get("update_commands", [])
+    except (requests.exceptions.RequestException, ValueError):
+        print("Unable to fetch version data from GitHub.")
+        return
+
+    if current_version != github_version:
+        print("An update is available.")
+
+        for file in update_files:
+            download_url = f"https://raw.githubusercontent.com/EletrixTimeYT/TimeWeb/main/{file}"
+            try:
+                response = requests.get(download_url, stream=True)
+                response.raise_for_status()
+                with open(file, "wb") as f:
+                    response.raw.decode_content = True
+                    shutil.copyfileobj(response.raw, f)
+            except (requests.exceptions.RequestException, IOError):
+                print(f"Unable to download {file} from GitHub.")
+
+        if update_libs:
+            print("Updating libraries...")
+            for lib in update_libs_list:
+                subprocess.run(["pip", "install", "--upgrade", lib])
+            print("Libraries update completed.")
+
+        if update_commands:
+            print("Executing update commands...")
+            execute_commands(update_commands)
+            print("Commands execution completed.")
+
+        if install:
+            print("Installing files...")
+            install_files()
+            print("Files installation completed.")
+
+        update_local_version(github_version)  # Mettre à jour la version locale dans le fichier version.yaml
+        print("Update completed.")
+    else:
+        print("No update available.")
+
+def main():
+    parser = argparse.ArgumentParser(description="Check for updates and perform upgrades.")
+    parser.add_argument("--upgrade", action="store_true", help="Upgrade the application if an update is available.")
+    parser.add_argument("--update-libs", action="store_true", help="Update libraries listed in version.yaml.")
+    parser.add_argument("--install", action="store_true", help="Install files from GitHub.")
+    args = parser.parse_args()
+
+    if args.upgrade:
+        check_update(update_libs=args.update_libs, install=args.install)
+    elif args.install:
+        install_files()
+    else:
+        print("Please provide the --upgrade or --install flag to perform an upgrade or install.")
+
+if __name__ == "__main__":
+    main()
